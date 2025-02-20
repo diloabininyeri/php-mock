97f55e9618229ca0473a81195fceb47913a265e4
Dilo surucu <berxudar@gmail.com>
2025-02-20 18:36:03 -0500

    To demonstrate and test that type hinting works properly, the Guarantee Type HintingTest class has been added to the tests.

---
+++ tests/unit/GuaranteeTypeHintingTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Zeus\Mock\Tests\unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use Zeus\Mock\MockFactory;
+use Zeus\Mock\Tests\stubs\User;
+use Zeus\Mock\Tests\stubs\UserRepositoryInterface;
+
+/**
+ *
+ */
+class GuaranteeTypeHintingTest extends TestCase
+{
+
+    /**
+     * @throws ReflectionException
+     */
+    #[Test]
+    public function testUserTypeHinting(): void
+    {
+        $mockFactory = new MockFactory();
+        $mockFactory->mockMethod('getId', function () {
+            return 999;
+        });
+
+        $mockUser = $mockFactory->createMock(User::class);
+
+        $this->assertEquals(999, $this->getId($mockUser));
+    }
+
+    /**
+     * @param User $user
+     * @return int
+     */
+    private function getId(User $user): int
+    {
+        return $user->getId();
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    #[Test]
+    public function testInterfaceTypeHinting(): void
+    {
+        $mockFactory = new MockFactory();
+
+        $mockFactory->mockMethod('getById', function () {
+            return 999;
+        });
+
+        $mockUserRepository = $mockFactory->createMock(UserRepositoryInterface::class);
+        $this->assertEquals(999, $this->getIdByInterface($mockUserRepository));
+    }
+
+    /**
+     * @param UserRepositoryInterface $userRepository
+     * @return int
+     */
+    private function getIdByInterface(UserRepositoryInterface $userRepository): int
+    {
+        return $userRepository->getById(1);
+    }
+}
