e67c33f965129a4e48557c0c002110f3970ec4b7
Dilo surucu <berxudar@gmail.com>
2025-02-21 20:14:22 -0500

    MockFunction object has been created. It can replace php's bin-built functions. I will add instructions on how to use it to the readme file.

---
+++ src/MockFunction.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Zeus\Mock;
+
+use Closure;
+
+/**
+ * @method static callFunction(string $name, array $args)
+ * @method static bool runningScope()
+ */
+class MockFunction
+{
+    /**
+     * @var array
+     */
+    private array $functions = [];
+    /**
+     * @var bool
+     */
+    public bool $isScoped = false;
+    /**
+     * @var bool
+     */
+    private bool $isBuilt = false;
+    /**
+     * @var MockFunction
+     */
+    private static self $instances;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        static::$instances = $this;
+    }
+
+    /**
+     * @param string $name
+     * @param Closure $function
+     * @return void
+     */
+    public function add(string $name, Closure $function): void
+    {
+        $this->functions[$name] = $function;
+    }
+
+
+    /**
+     * @param string $name
+     * @param array $args
+     * @return mixed
+     */
+    public function call(string $name,array $args): mixed
+    {
+        $func = $this->functions[$name];
+        return $func(...$args);
+    }
+
+    /**
+     * @param string $namespace
+     * @return string
+     */
+    private function generate(string $namespace): string
+    {
+        $code = "namespace $namespace;\n\n";
+
+        foreach ($this->functions as $name => $_) {
+            $code .= "function $name() {\n";
+            $code .= "    if (\Zeus\Mock\MockFunction::runningScope()) {\n";
+            $code .= "        return \Zeus\Mock\MockFunction::callFunction('$name',func_get_args());\n";
+            $code .= "    }\n";
+            $code .= "    return \\$name();\n";
+            $code .= "}\n";
+        }
+
+        return $code;
+    }
+
+    public function scope(string $namespace): void
+    {
+        $this->isScoped = true;
+        if (!$this->isBuilt) {
+            eval($this->generate($namespace));
+            $this->isBuilt = true;
+        }
+    }
+
+
+    public function endScope():void
+    {
+        $this->isScoped = false;
+    }
+    /**
+     * @param string $method
+     * @param array $arguments
+     * @return mixed|void
+     */
+    public static function __callStatic(string $method, array $arguments)
+    {
+        return match ($method) {
+            'callFunction' => static::$instances->call(...$arguments),
+            'runningScope' => static::$instances->isScoped,
+            default => null,
+        };
+    }
+}
---
+++ tests/unit/MockFunctionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Zeus\Mock\Tests\unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Zeus\Mock\MockFunction;
+
+class MockFunctionTest extends TestCase
+{
+
+
+    #[Test]
+    public function time(): void
+    {
+
+        $m = new MockFunction();
+        $m->add('time', function () {
+            return 100;
+        });
+        $m->scope(__NAMESPACE__);
+        $this->assertEquals(100, time());
+
+        $m->endScope();
+
+        $this->assertNotEquals(100, time());
+
+    }
+
+    #[Test]
+    public function sleep(): void
+    {
+
+        $m = new MockFunction();
+        $m->add('sleep', function (int $m) {
+            return $m;
+        });
+        $m->scope(__NAMESPACE__);
+        $this->assertEquals(10, sleep(10));
+        $m->endScope();
+    }
+}
