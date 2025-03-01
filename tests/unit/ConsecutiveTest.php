3bc00851d222aa5ace3c41c6b9f245f53bf0e944
Dilo surucu <berxudar@gmail.com>
2025-02-28 21:01:20 -0500

    the test class has added for the consecutive method

---
+++ tests/unit/ConsecutiveTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Zeus\Mock\Tests\unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Zeus\Mock\MockFunction;
+use Zeus\Mock\Tests\stubs\Date;
+
+class ConsecutiveTest extends TestCase
+{
+
+
+    #[Test]
+    public function date():void
+    {
+        $mockFunction = new MockFunction();
+        $mockFunction->addConsecutive('date',
+            [
+                '2022-01-01',
+                '2022-01-02',
+                '2022-01-03'
+            ]);
+
+        $mockFunction->runWithMock(new Date(),function (Date $date)
+        {
+            $this->assertEquals('2022-01-01', $date->now());
+            $this->assertEquals('2022-01-02', $date->now());
+            $this->assertEquals('2022-01-03', $date->now());
+        });
+    }
+}
