585495077126ff0e20dd48621609f8be261e264c
Dilo surucu <berxudar@gmail.com>
2025-02-28 21:19:38 -0500

    MockFactory::addConsecutive has been added and its tests

--- src/MockFactory.php
+++ src/MockFactory.php
@@ -21,6 +21,10 @@ class MockFactory
         $this->mockMethod->mockMethod('object.on.created', fn(...$args) => null);
     }
 
+    /**
+     * @param MockMethod $mockMethod
+     * @return self
+     */
     public static function from(MockMethod $mockMethod):self
     {
         return new self($mockMethod);
@@ -67,6 +71,10 @@ class MockFactory
             str_replace('.', '', uniqid(time(), true));
     }
 
+    /**
+     * @param Closure $closure
+     * @return $this
+     */
     public function onMockInstanceCreated(Closure $closure):self
     {
         $this->mockMethod->mockMethod('object.on.created', $closure);
@@ -83,13 +91,32 @@ class MockFactory
         return $this->mockMethod->$method(...$arguments);
     }
 
+    /**
+     * @param string $methodName
+     * @return int
+     */
     public function getCallCount(string $methodName):int
     {
         return $this->mockMethod->getCallCount($methodName);
     }
 
+    /**
+     * @return MockMethodInterface
+     */
     public function getMockMethod(): MockMethodInterface
     {
         return $this->mockMethod;
+    }
+
+    /**
+     * @param string $methodName
+     * @param array $returns
+     * @return void
+     */
+    public function addConsecutive(string $methodName, array $returns):void
+    {
+        $this->mockMethod->mockMethod($methodName, function () use (&$returns) {
+            return array_shift($returns);
+        });
     }
 }
